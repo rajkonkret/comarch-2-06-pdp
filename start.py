9f092ef1432b5e151a8ae7b5885e6d213eca2a30
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-06-07 14:31:06 +0200

    add start.py - do pakietu

---
+++ start.py
@@ -0,0 +1,12 @@
+import pakiet
+import pakiet.fun_pakiet as pk
+from pakiet import fun_pakiet
+from pakiet import *  # troche taki import statyczny
+
+pk.powitanie()
+fun_pakiet.powitanie()
+pakiet.powitanie()  # po umozliwieniu importu w pliku __init__.py
+fun_pakiet.pozegnanie()
+pk.pozegnanie()
+powitanie()
+info()
