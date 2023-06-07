d0883eaa382e05d2cd4beb42bf57d4d26c6b7749
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-06-07 12:49:22 +0200

    add generatory

---
+++ generatory.py
@@ -0,0 +1,27 @@
+def kwadrat(n):
+    for x in range(n):
+        print(x ** 2)
+
+
+def kwa2(n):
+    for x in range(n):
+        yield x ** 2  # zwraca wynik i zapamietuje w ktorym miejscu sie zatrzymał
+
+
+kwadrat(5)  # 0..4 liczy kwadraty
+kwa = kwa2(5)
+print(kwa)  # <generator object kwa2 at 0x104e0f510>
+print(next(kwa))
+print(next(kwa))
+print(next(kwa))  # 4
+print("wypisac cokolwiek")
+print("zrobic cokolwiek")
+lista = []
+lista.append(4)
+print(next(kwa))  # 9
+print(next(kwa))  # 16
+kwa_2 = kwa2(9)
+print(next(kwa_2))
+print(next(kwa_2))
+print(next(kwa_2))
+print(next(kwa_2))  # 9
